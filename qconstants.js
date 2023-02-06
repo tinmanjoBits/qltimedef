d874fe813392da6a92f3df32ba17c644e946b2e3
tinmanjoBits <dryanemail@gmail.com>
2023-02-06 12:42:44 +0000

    bug fix

--- qconstants.js
+++ qconstants.js
@@ -3,8 +3,8 @@
 const MAX_EPISODES = 100;
 
 // hypervaribles
-const ALPHA = 0.2; // learning rate
-const GAMMA = 0.75; // discount function
-const EPSILON = 0.5; // rate of exploration
+const ALPHA = 0.4; // learning rate
+const GAMMA = 0.9; // discount function
+const EPSILON = 1.0; // rate of exploration
 
 const REWARD_SCALE = 50;
